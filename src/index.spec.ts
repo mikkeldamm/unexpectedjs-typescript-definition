b201a5c1f8eb48065861064bef340cd8d5fec689
Mikkel Richter Damm <mrd@one.com>
2016-07-05 16:32:01 +0200

    mocha tests using the new unexpected.d.ts added

---
+++ src/index.spec.ts
@@ -0,0 +1,21 @@
+import expect = require('unexpected');
+
+import { Calculator } from './index';
+
+
+describe('test unexpected expectations:', () => {
+    
+    it('<thruthy> of all', () => {
+
+        expect(true, 'to be ok');
+        expect(1, 'to be ok');
+        expect("1", 'to be ok');
+        expect({}, 'to be ok');
+    });
+
+    it('<strings>', () => {
+
+        expect('mikkel d3mm.', 'to match', /d+.?/gi);
+        expect('mikke', 'to have length', 2);
+    });
+});
